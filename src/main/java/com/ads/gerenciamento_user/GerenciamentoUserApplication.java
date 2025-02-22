9c1ccd7febc209f70ce19512ae1ca4fe731c3b4a
Ygor Nacif <ygor.nacif00@gmail.com>
2025-02-22 17:27:28 -0300

    first commit

---
+++ src/main/java/com/ads/gerenciamento_user/GerenciamentoUserApplication.java
@@ -0,0 +1,13 @@
+package com.ads.gerenciamento_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoUserApplication.class, args);
+	}
+
+}
