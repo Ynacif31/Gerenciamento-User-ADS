a46bfec37454798d98236da3f324d152f793290f
Ygor Nacif <ygor.nacif00@gmail.com>
2025-03-15 17:55:01 -0300

    feat: Entidade user criada e implementada

--- src/main/java/com/ads/gerenciamento_user/GerenciamentoUserApplication.java
+++ src/main/java/com/ads/gerenciamento_user/GerenciamentoUserApplication.java
@@ -1,5 +1,6 @@
 package com.ads.gerenciamento_user;
 
+import com.ads.gerenciamento_user.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class GerenciamentoUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciamentoUserApplication.class, args);
+
 	}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,5 +20,19 @@ CREATE TABLE IF NOT EXISTS tb_user_roles (
     FOREIGN KEY (role_id) REFERENCES tb_roles(id) ON DELETE CASCADE
     );
 
+-- Insert user with a hashed password (example using bcrypt hash)
 INSERT INTO tb_user (username, email, password, enabled)
-VALUES ('ygornacif', 'ygor@gmail.com', 'ygornacif', TRUE)
+VALUES ('ygornacif', 'ygor@gmail.com', '$2a$10$7eqJtq98hPqEX7fNZaFWoOZt1r8sJ5FJxXH1e6UzG/Kjz7p/JN9eW', TRUE);
+
+-- Insert roles
+INSERT INTO tb_roles (name)
+VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+
+-- Assign roles to the user
+INSERT INTO tb_user_roles (user_id, role_id)
+VALUES
+    ((SELECT id FROM tb_user WHERE username = 'ygornacif'),
+     (SELECT id FROM tb_roles WHERE name = 'ROLE_USER')),
+    ((SELECT id FROM tb_user WHERE username = 'ygornacif'),
+     (SELECT id FROM tb_roles WHERE name = 'ROLE_ADMIN'));
+
